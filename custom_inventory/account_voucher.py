db1ec2eb3caaf6f992c33b23f5df85855abfafa4
parkash067 <pk_bscs@yahoo.com>
2018-07-19 21:56:59 +0500

    fixes

--- custom_inventory/account_voucher.py
+++ custom_inventory/account_voucher.py
@@ -24,5 +24,13 @@ class account_invoice(osv.osv):
                     line.account_id = line.product_id.property_account_expense
                 else:
                     raise except_orm(_('Error!'), _('Kindly configure expense head against this product'))
+
+        if self.state == 'draft' and self.partner_id.customer:
+            self.account_id = self.partner_id.property_account_receivable.id
+            for line in self.invoice_line:
+                if line.product_id.property_account_income:
+                    line.account_id = line.product_id.property_account_income
+                else:
+                    raise except_orm(_('Error!'), _('Kindly configure expense head against this product'))
         else:
             raise except_orm(_('Error!'), _('You can reset accounts head of invoice only in draft state'))
