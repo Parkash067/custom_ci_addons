4423b5f39ef7f39c1a52c594a61b0f33fd9e3348
parkash067 <pk_bscs@yahoo.com>
2018-07-22 17:05:39 +0500

    add writeoff

--- multiple write-off/write_off.py
+++ multiple write-off/write_off.py
@@ -114,7 +114,7 @@ class writeoff_amount(osv.osv):
                     'date': voucher.date,
                     'credit': diff > 0 and diff or 0.0,
                     'debit': diff < 0 and -diff or 0.0,
-                    'amount_currency': company_currency <> current_currency and (sign * -1 * voucher.writeoff_amount) or 0.0,
+                    'amount_currency': company_currency <> current_currency and (sign * -1 * line_total) or 0.0,
                     'currency_id': company_currency <> current_currency and current_currency or False,
                     'analytic_account_id': voucher.analytic_id and voucher.analytic_id.id or False,
                 }
@@ -213,6 +213,8 @@ class writeoff_amount(osv.osv):
                                                                          company_currency, current_currency, context)
 
                 # Create the writeoff line if needed  voucher.writeoff_acc_id.id
+                print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.total>>>>>>>>>>>>>>>>>>>>>>>>>>>>",line_total)
+
                 for multipart_line in obj.write_off_line:
                     ml_writeoff = self.writeoff_move_line_get(cr, uid, voucher.id, multipart_line.account.id,
                                                               multipart_line.writeoff_amount, multipart_line.comment,obj.payment_method,move_id, name,
@@ -230,13 +232,24 @@ class writeoff_amount(osv.osv):
                 if voucher.journal_id.entry_posted:
                     move_pool.post(cr, uid, [move_id], context={})
                 # We automatically reconcile the account move lines.
+                _rec_ids=[]
                 reconcile = False
                 for rec_ids in rec_list_ids:
                     if type(rec_ids) is int or type(rec_ids) is list and len(rec_ids)>=2:
-                        reconcile = move_line_pool.reconcile_partial(cr, uid, rec_ids,
-                                                                     writeoff_acc_id=voucher.writeoff_acc_id.id,
-                                                                     writeoff_period_id=voucher.period_id.id,
-                                                                     writeoff_journal_id=voucher.journal_id.id)
+                        if obj.multi_counter_parts:
+                            for multipart_line in obj.write_off_line:
+                                print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",rec_ids)
+                                _rec_ids.append(rec_ids)
+                                if len(_rec_ids)>=2:
+                                    reconcile = move_line_pool.reconcile_partial(cr, uid, _rec_ids,
+                                                                             writeoff_acc_id=multipart_line.account.id,
+                                                                             writeoff_period_id=voucher.period_id.id,
+                                                                             writeoff_journal_id=voucher.journal_id.id)
+                        else:
+                            reconcile = move_line_pool.reconcile_partial(cr, uid, rec_ids,
+                                                                         writeoff_acc_id=voucher.writeoff_acc_id.id,
+                                                                         writeoff_period_id=voucher.period_id.id,
+                                                                         writeoff_journal_id=voucher.journal_id.id)
             return True
         else:
             if context is None:
@@ -287,6 +300,7 @@ class writeoff_amount(osv.osv):
                 reconcile = False
                 for rec_ids in rec_list_ids:
                     if len(rec_ids) >= 2:
+                        print(">>>>>>>>>>>>>>>>>>>>>>>Rec_ids>>>>>>>>>>>>>>>>>>>>>>>>>>",rec_ids)
                         reconcile = move_line_pool.reconcile_partial(cr, uid, rec_ids, writeoff_acc_id=voucher.writeoff_acc_id.id, writeoff_period_id=voucher.period_id.id, writeoff_journal_id=voucher.journal_id.id)
             return True
 
